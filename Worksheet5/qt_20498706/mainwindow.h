6ccdd1ece71673817f6cbdbd11fe7ae0c43cb5e0
Eakkorn <125054898+Eakkorn@users.noreply.github.com>
2023-02-16 16:26:31 +0000

    Worksheet5

---
+++ Worksheet5/qt_20498706/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "ModelPartList.h"
+#include "ModelPart.h"
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+ public slots:
+    void handleButton();
+    void handleTreeClicked();
+
+
+signals:
+   void statusUpdateMessage( const QString & message, int timeout);
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_actionOpen_File_triggered();
+
+private:
+    Ui::MainWindow *ui;
+    ModelPartList *partList;
+};
+#endif // MAINWINDOW_H
