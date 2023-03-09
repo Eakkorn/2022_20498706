91760aa72c705867daa2f59bdea3ed584008274a
Eakkorn <125054898+Eakkorn@users.noreply.github.com>
2023-03-09 16:26:31 +0000

    test doc

--- Worksheet7/adder.h
+++ Worksheet7/adder.h
@@ -1,15 +1,7 @@
-// begin =adder.h=
-// The following two lines prevent adder.h from being included
-// more than once in any source ( .cpp ) file. If this were to happen
-// it would cause problems in the compile process but it is difficult to
-// prevent manually inlarge  projects. These #ifndef , #define and #endif
-// commands for an ” include guard” and are types of compiler directive .
-// The include guard contains an ID for this file "MATHSLIB_ADDER_H" , this is
-// arbitrary but must be unique within the project.
 #ifndef MATHSLIB_ADDER_H
 #define MATHSLIB_ADDER_H
 
-/** @ file
+/** @file
 * This file contains the declarations of all exported functions in the maths library.
 */
 
